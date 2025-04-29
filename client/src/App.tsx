2402fef57d28db3e836012a0e62290a3ed5b776b
Cameron Proulx <cameronproul@umass.edu>
2025-04-29 19:23:01 -0400

    Added Clerk boilerplate to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
