a04dfc08169bc680562fd98476577346f92321a6
Cameron Proulx <cameronproul@umass.edu>
2025-04-29 18:26:07 -0400

    Added userId endpoint for testing

--- server/app.py
+++ server/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import os
+import warnings
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from clerk_backend_api import Clerk
+from clerk_backend_api.jwks_helpers import AuthenticateRequestOptions
+
+# Suppress specific warning from Clerk SDK
+warnings.filterwarnings(
+    'ignore', 
+    message='authenticate_request method is applicable in the context of Backend APIs only.'
+)
 
 load_dotenv()
 
@@ -14,3 +23,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/api/userId')
+def user():
+    sdk = Clerk(bearer_auth=os.getenv('CLERK_SECRET_KEY'))
+    request_state = sdk.authenticate_request(
+        request,
+        AuthenticateRequestOptions(
+            authorized_parties=['https://example.com', 'http://localhost', 'http://127.0.0.1']
+        )
+    )
+    if request_state.is_signed_in:
+        return {'userId': request_state.user_id}, 200
+    else:
+        return {'error': 'User not signed in'}, 401
