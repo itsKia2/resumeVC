4329d95f2019848085af972e09008e06e52bc6a8
Cameron Proulx <cameronproul@umass.edu>
2025-04-29 17:53:38 -0400

    Added Supabase connection to backend

--- server/app.py
+++ server/app.py
@@ -1,4 +1,13 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 
