f0e765d1e900e6c51d77576bfeaec459d295152e
Cameron Proulx <cameronproul@umass.edu>
2025-05-15 18:35:30 -0400

    Consolidated signedIn & signedOut tests into auth

--- client/e2e/auth.spec.ts
+++ client/e2e/auth.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { clerk } from '@clerk/testing/playwright'
 
-test('Test clicking Resumes Link', async ({ page }) => {
+test('Test clicking Resumes link signed in', async ({ page }) => {
   await page.goto('/');
   await clerk.signIn({
     page,
@@ -13,4 +13,10 @@ test('Test clicking Resumes Link', async ({ page }) => {
   })
   // User must be signed in to see the resumes link
   await page.getByRole('link', { name: 'Resumes' }).click();
+});
+
+test('Test visiting resumes page logged out', async ({ page }) => {
+  await page.goto('/categories');
+  // A signed out user should not be able to see the resumes page, rather they should see an alert to sign in
+  await expect(page.getByRole('alert')).toContainText('Please log in to view this page.');
 });
