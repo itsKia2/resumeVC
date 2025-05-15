a0d6cf6f86ab9118426593b13a433a9432d2deca
Cameron Proulx <cameronproul@umass.edu>
2025-05-15 18:33:58 -0400

    Removed CollectionView (Replaced with Categories)

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '@/components/ui/Layout';
 
-import CollectionView from '@/pages/CollectionView';
 import JobAnalysis from '@/pages/JobAnalysis';
 import UserProfile from '@/pages/UserProfile';
 import Login from '@/pages/Login';
@@ -18,7 +17,6 @@ const Router: React.FC = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<App />} />
-        <Route path="collections" element={<CollectionView />} />
         <Route path="job-analysis" element={<JobAnalysis />} />
         <Route path="profile" element={<UserProfile />} />
         <Route path="login" element={<Login />} />
